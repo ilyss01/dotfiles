642624925b5a1344725f9d70f4a73fc86975c195
Ilya Romanuk <47274578+ilyss01@users.noreply.github.com>
2021-10-18 10:56:38 +0000

    Add files via upload

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+polybar top &
+echo "Bars launched..."
