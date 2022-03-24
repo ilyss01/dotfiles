93f4e6aa6d6b6242505270c53f6d83cee8de541c
Ilya Romanuk <ilya.romanuck2014@yandex.ru>
2022-03-24 17:01:48 +0300

    Moving config files to stow. Migration xorg -> wayland

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,15 @@
+function fish_greeting
+end
+
+set -gx EDITOR nvim
+
+# Start X at login
+if status --is-interactive
+  if test -z "$DISPLAY" -a $XDG_VTNR = 1
+    exec sway 
+  end
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
